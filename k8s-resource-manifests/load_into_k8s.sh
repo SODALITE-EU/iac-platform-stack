f2825d6d8f2b20b2588ef2b56949740a22015e07
Steven Presser <steven.presser@hlrs.de>
2021-09-17 11:42:48 +0200

    Add keycloak admin password

--- k8s-resource-manifests/load_into_k8s.sh
+++ k8s-resource-manifests/load_into_k8s.sh
@@ -6,24 +6,31 @@ uuid() {
     python3 -c 'import uuid; print(uuid.uuid4())'
 }
 
+genpw() {
+    openssl rand -base64 16
+}
+
 set -xe
 
 if [ ! -f .env ]
 then
     export CLIENT_SECRET=$(uuid)
     export VAULT_TOKEN=$(uuid)
+    export KEYCLOAK_ADMIN_PASSWORD=$(genpw)
 
     envsubst < .env.tmpl > .env
 fi
 
 source .env
 
-envsubst < keycloak-secret.yaml.tmpl > keycloak-secret.yaml
-envsubst < vault.yaml.tmpl > vault.yaml
+for TMPL in $(find . -name '*.tmpl')
+do
+    envsubst < $TMPL > $(echo $TMPL | sed 's/.tmpl//' )
+done
 
 $KUBECTL apply -f namespace-sodalite-services.yaml
 
-$KUBECTL apply -f keycloak-secret.yaml
-$KUBECTL apply -f keycloak.yaml
-
-$KUBECTL apply -f vault.yaml
+for YAML in $(find . -name '*.yaml')
+do
+    $KUBECTL apply -f $YAML
+done
