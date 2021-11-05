e90f5bf791a31c1d34b243a2258a3513ed27f54e
Steven Presser <steven.presser@hlrs.de>
2021-11-05 09:18:02 +0100

    Remove whoami from service list

--- k8s-resource-manifests/load_into_k8s.sh
+++ k8s-resource-manifests/load_into_k8s.sh
@@ -113,7 +113,7 @@ kubectl -n kube-system delete pod -l app.kubernetes.io/name=traefik
 
 # And start applying other services. k8s will take care of anything that
 # isn't quite up/in the wrong order, so we can just batch apply things
-for CDIR in keycloak vault-secret-uploader xopera-postgres xopera-rest-api iac-builder knowledge-db semantic-web tosca-smells consul alertmanager prometheus ruleserver prometheus-skydive-connector skydive-analyzer registry whoami
+for CDIR in keycloak vault-secret-uploader xopera-postgres xopera-rest-api iac-builder knowledge-db semantic-web tosca-smells consul alertmanager prometheus ruleserver prometheus-skydive-connector skydive-analyzer registry
 do
 for YAML in $(find $CDIR -name '*.yaml')
 do
